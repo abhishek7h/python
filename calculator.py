316a1548df5c1cde1c438421256687f28ed506b5
Abhishek H <abh1sh3kh@gmail.com>
2022-03-05 20:57:02 +0530

    changes/usr/bin/python3.10 /home/abhishek/Documents/Code/python/calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,22 @@
 num1 = int(input("Enter the first number that you want to calculate: "))
 num2 = int(input("Enter the second number that you want to calculate: "))
-operator = (input("Enter either [+, -, * or /] "))
+operator = input("Enter '+', '-', '*' or '/' ")
 
-if operator == '+' :
-    print(num1 + num2)
-    
-    
-elif operator == '-' :
-    print(num1 - num2)
-    
-    
-elif operator == '*' :
-    print(num1 * num2)
-    
-    
-elif operator == '/' : 
-    print(num1 / num2)
+
+if operator == '+':
+    answer = num1 + num2
+
+
+if operator == '-':
+    answer = num1 - num2
+
+
+if operator == '*':
+    answer = num1 * num2
+
+
+if operator == '/':
+    answer = num1 / num2
+
+
+print("The answer is " + str(answer))
