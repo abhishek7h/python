6163543b36f45ad2b94598cdd8dd676b3b41ddfc
Abhishek.H <abhishek18h@gmail.com>
2022-05-31 20:37:37 +0530

    deleted music_player

--- music_player/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from playsound import playsound
-
-songName = input("Enter song name: ")
-
-while True:
-    if songName == "heat waves":
-        print("playing heat waves by glass animals...")
-        playsound("heat_waves.mp3")
-        break
