26f0c3958ac678d3e04c1494df09c8780e0cc4f4
Abhishek.H <abhishek18h@gmail.com>
2022-11-11 14:06:12 +0530

    added email slicer

---
+++ tictactoe.py
@@ -0,0 +1,24 @@
+def printBoard(xState, zState):
+    print(f' 0 | 1 | 2 ')
+    print(f' --|---|---')
+    print(f' 3 | 4 | 5 ')
+    print(f' --|---|---')
+    print(f' 6 | 7 | 8 ')
+    
+
+if __name__ == "__main__":
+        xState = [0, 0, 0, 0, 0, 0, 0, 0]
+        zState = [0, 0, 0, 0, 0, 0, 0, 0]
+        turn = 1 # 1 for x and 0 for
+        print("Welcome to Tic Tac Toe!")
+        while(True):
+            printBoard(xState, zState)
+            if(turn == 1):
+                value = int(input("Please enter a value"))
+                xState[value] = 1
+            else:
+                print("x's chance")
+                value = int(input("Please enter a value"))
+                zState[value] = 1
+            
+            break
