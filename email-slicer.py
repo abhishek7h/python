c8b723ec50cddb6142fbf1a53bcee2089ebc4a3b
Abhishek.H <abhishek18h@gmail.com>
2022-11-11 14:10:07 +0530

    added email slicer

--- email-slicer.py
+++ email-slicer.py
@@ -3,4 +3,4 @@ email = input("Enter your email: ").strip()
 username = email[:email.index('@')]
 domain = email[email.index('@') + 1:]
 
-print(f"Your username is {username} & domain is {domain}")
+print(f"Your username is {username} & domain is {domain}.")
