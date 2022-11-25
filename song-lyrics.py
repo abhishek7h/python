16cfffc8317aea94b6de21ef8cb5c4131249f933
Abhishek.H <abhishek18h@gmail.com>
2022-11-25 21:25:32 +0530

    added song-lyrics

---
+++ song-lyrics.py
@@ -0,0 +1,192 @@
+print("Toxic, Heat Waves, Midnight Rain")
+song = input("Enter the name of a song from the above to receive its lyrics: ")
+
+if song == "Toxic":
+    print("""All my friends are toxic, all ambitionless
+So rude and always negative
+I need new friends, but it's not that quick and easy
+Oh, I'm drowning, let me breathe
+I'm better off all by myself
+Though I'm feeling kinda empty without somebody else
+Oh, I hear you crying out for help
+But you never showed for me when I was ringing your cellphone
+Oh, you don't know how it feels to be alone
+Baby, oh, I'll make you know, I'll make you know, oh
+I'm drowning, let me breathe
+I'm drowning, let me breathe
+I'm drowning, let me breathe
+I'm drowning, let me breathe
+But life is immaculate, backing it up a bit
+Counting my hours and knocking on wood
+Avoiding my opposites, chewin' on chocolate
+Had a bit limited time, but I should
+Be good for a minute, don't want to admit it
+I'm running on seconds, I'm rigid, I'm screwed
+Don't know what to do, I'm thinking of you
+I'm drinking up bottles and bottles of booze
+I'm better off all by myself
+Though I'm feeling kinda empty without somebody else
+Oh, I hear you crying out for help
+But you never showed for me when I was ringing your cellphone
+Oh, you don't know how it feels to be alone
+Baby, oh, I'll make you know, I'll make you know, oh
+I fell into your river
+That's where you told me lies
+You said that I'd feel better
+But this is where good guys die
+You took my pride away, but
+You cannot take my life
+I'll find another way
+I'll wonder if you're takin' my life
+Don't you see how I...
+I'm better off all by myself
+Though I'm feeling kinda empty without somebody else
+Oh, I hear you crying out for help
+But you never showed for me when I was ringing your cellphone
+Oh, you don't know how it feels to be alone
+Baby, oh, I'll make you know, I'll make you know, oh
+I'm better off all by myself
+Though I'm feeling kinda empty without somebody else
+Oh, I hear you crying out for help
+But you never showed for me when I was ringing your cellphone
+Oh, you don't know how it feels to be alone
+Baby, oh, I'll make you know, I'll make you know, oh
+    """)
+
+elif song == "Heat Waves":
+    print("""
+    Road shimmer
+Wiggling the vision
+Heat heat waves
+I'm swimming in a mirror
+Road shimmer
+Wiggling the vision
+Heat heat waves
+I'm swimming in a
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now
+Usually I put
+Something on TV
+So we never think
+About you and me
+But today I see
+Our reflections
+Clearly in Hollywood
+Laying on the screen
+You just need a better life than this
+You need something I can never give
+Fake water all across the road
+It's gone now, the night has come, but
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now
+You can't fight it
+You can't breathe
+You say something so loving, but
+Now I've got to let you go
+You'll be better off in someone new
+I don't wanna be alone
+You know it hurts me too
+You look so broken when you cry
+One more and then I'll say goodbye
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now
+I just wonder what you're dreaming of
+When you sleep and smile so comfortable
+I just wish that I could give you that
+That look that's perfectly un-sad
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Heat waves been faking me out
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now
+Sometimes, all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now
+Road shimmer
+Wiggling the vision
+Heat heat waves
+I'm swimming in a mirror
+Road shimmer
+Wiggling the vision
+Heat heat waves
+I'm swimming in a mirror
+    """)
+
+elif song == "Midnight Rain":
+    print("""
+    Rain, he wanted it comfortable
+I wanted that pain
+He wanted a bride
+I was making my own name
+Chasing that fame
+He stayed the same
+All of me changed like midnight
+My town was a wasteland
+Full of cages, full of fences
+Pageant queens and big pretenders
+But for some, it was paradise
+My boy was a montage
+A slow-motion, love potion
+Jumping off things in the ocean
+I broke his heart 'cause he was nice
+He was sunshine, I was midnight rain
+He wanted it comfortable
+I wanted that pain
+He wanted a bride
+I was making my own name
+Chasing that fame
+He stayed the same
+All of me changed like midnight
+It came like a postcard
+Picture perfect, shiny family
+Holiday, peppermint candy
+But for him it's every day
+So I peered through a window
+A deep portal, time travel
+All the love we unravel
+And the life I gave away
+'Cause he was sunshine
+I was midnight rain
+He wanted it comfortable
+I wanted that pain
+He wanted a bride
+I was making my own name
+Chasing that fame
+He stayed the same
+All of me changed
+Like midnight
+Rain, he wanted it comfortable
+I wanted that pain
+He wanted a bride
+I was making my own name
+Chasing that fame
+He stayed the same
+All of me changed
+Like midnight
+I guess sometimes we all get
+Just what we wanted, just what we wanted
+And he never thinks of me
+Except when I'm on TV
+I guess sometimes we all get
+Some kind of haunted, some kind of haunted
+And I never think of him
+Except on midnights like this (midnights like this)
+    """)
