bc5eece14f26c38102d845f914386c4a9006a19b
Abhishek.H <abhishek18h@gmail.com>
2022-11-25 18:53:45 +0530

    yo

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@ num1 = int(input("Enter the first number that you want to calculate: "))
 operator = input("Enter '+', '-', '*' or '/' ")
 num2 = int(input("Enter the second number that you want to calculate: "))
 
+#logic
 
 if operator == '+':
     answer = num1 + num2
