c79fb5e6c84c8cd086c9b55d730551c1b0d78ae7
Abhishek.H <abhishek18h@gmail.com>
2022-11-25 21:47:52 +0530

    added a dating simulator for you single ppl (includes me) </3

---
+++ games/relationship-simulator.py
@@ -0,0 +1,7 @@
+import random
+
+texts = ["If you were a triangle, you would be aCute triangle!", "Did you sleep well?", "How was your day?", "I love you!", "ily so much <3", "cutie pie!", "I love you!"]
+text = random.choice(texts)
+print(text)
+reply = input("Leave a reply: ")
+print("<3")
