a6764c76916517dbe2faf48a848d2db1bf7084c8
Abhishek.H <abhishek18h@gmail.com>
2022-12-20 20:40:15 +0530

    looped calculator

--- math/calculator.py
+++ math/calculator.py
@@ -1,23 +1,32 @@
-num1 = int(input("Enter the first number that you want to calculate: "))
-operator = input("Enter '+', '-', '*' or '/' ")
-num2 = int(input("Enter the second number that you want to calculate: "))
+def main():
 
-#logic
+    num1 = int(input("Enter the first number that you want to calculate: "))
+    operator = input("Enter '+', '-', '*' or '/' ")
+    num2 = int(input("Enter the second number that you want to calculate: "))
 
-if operator == '+':
-    answer = num1 + num2
+    #logic
 
-
-if operator == '-':
-    answer = num1 - num2
+    if operator == '+':
+        answer = num1 + num2
 
 
-if operator == '*':
-    answer = num1 * num2
+    if operator == '-':
+        answer = num1 - num2
 
 
-if operator == '/':
-    answer = num1 / num2
+    if operator == '*':
+        answer = num1 * num2
 
 
-print("The answer is " + str(answer))
+    if operator == '/':
+        answer = num1 / num2
+
+
+    print("The answer is " + str(answer))
+
+    rerun = input("Do you want to run again? (y/n) ").lower()
+    if rerun == 'y':
+     main()
+    elif rerun == 'n':
+     exit()
+main()
