af7e2eb4c6a55b13dbdeb3ca65d70aa343d0d671
Abhishek.H <abhishek18h@gmail.com>
2022-12-27 20:01:38 +0530

    added ETH in currency converter

--- utility/currency-converter-basic.py
+++ utility/currency-converter-basic.py
@@ -1,30 +1,41 @@
 def bruh():
-    list = ['USD', 'EUR', 'GBP', 'BTC', 'CAD']
+    list = ['USD', 'EUR', 'GBP', 'BTC', 'CAD', 'ETH']
     print(list)
     currency = input("Enter the currency you want to convert to INR: ")
+
     if currency == "USD":
-        usd = int(input("> Enter USD ($): "))
+        usd = float(input("> Enter USD ($): "))
         inr = usd * 82.85
         print(f"${usd} is ₹{inr} ")
+
     elif currency == "EUR":
-        euro = int(input("Enter EURO (€): "))
+        euro = float(input("Enter Euros (€): "))
         inr = euro * 88.33
         print(f"€{euro} is ₹{inr} ")
+
     elif currency == "GBP":
-        gbp = int(input("Enter POUND STERLING (£): "))
+        gbp = float(input("Enter Pound Sterlings (£): "))
         inr = gbp * 99.93
         print(f"£{gbp} is ₹{inr} ")
+        
     elif currency == "BTC":
-        btc = int(input("Enter BTC (₿): "))
+        btc = float(input("Enter Bitcoin (₿): "))
         inr = btc * 1395644.23
         print(f"₿{btc} is ₹{inr} ")
+
     elif currency == "CAD":
-        cad = int(input("Enter CAD ($): "))
+        cad = float(input("Enter Canadian Dollars ($): "))
         inr = cad * 61.23
         print(f"${cad} is ₹{inr} ")
-    repeat = input("Would you like to run again? ")
-    if repeat == "yes":
+
+    elif currency == "ETH":
+        eth = float(input("Enter Ethereum (Ξ): "))
+        inr = eth * 100854.11
+        print(f"Ξ{eth} is ₹{inr} ")
+        
+    repeat = input("Would you like to run again? y/n ")
+    if repeat == "y":
         bruh()
-    else: 
+    elif repeat == "n": 
         exit()
 bruh()
