5e9201ccb39ca13ff94cb79dff85761296e799e1
Abhishek.H <abhishek18h@gmail.com>
2022-12-27 15:41:00 +0530

    looped diceroller and agecalc

--- games/dice-roller.py
+++ games/dice-roller.py
@@ -1,4 +1,12 @@
-import random
+def main():
+    import random
 
-rolled = random.randint(1, 6)
-print(f"You rolled: {rolled}")
+    rolled = random.randint(1, 6)
+    print(f"> You rolled: {rolled}")
+
+    repeat = input("> Would you like to roll again? y/n ")
+    if repeat == "y":
+        main()
+    elif repeat == "n":
+        exit()
+main()
--- math/age-calc.py
+++ math/age-calc.py
@@ -1,10 +1,19 @@
-bday = int(input('In which year were you born? '))
+def main():
 
-age = 2022 - bday
+    bday = int(input('In which year were you born? '))
 
-print(f'You are {age} years old.')
+    age = 2022 - bday   
 
-if age >= 18:
-    print(f'PS: Since you are {age} years old, you are legally an adult. ')
-else:
-    print(f'PS: You are legally not an adult, since you are {age}.')
+    print(f'You are {age} years old.')
+
+    if age >= 18:
+        print(f'PS: Since you are {age} years old, you are legally an adult. ')
+    else:
+        print(f'PS: You are legally not an adult, since you are {age}.')
+    
+    repeat = input("> Would you like to run again? y/n ")
+    if repeat == "y":
+        main()
+    elif repeat == "n":
+        exit()
+main()
