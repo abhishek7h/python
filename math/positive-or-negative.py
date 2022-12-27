d540540c6f98d0a30065892e19396cc5553eeabb
Abhishek.H <abhishek18h@gmail.com>
2022-12-27 15:21:34 +0530

    looped positive-or-negative (math)

--- math/positive-or-negative.py
+++ math/positive-or-negative.py
@@ -1,7 +1,14 @@
-num = int(input("Enter the number you would like to check: "))
+def main():
+    num = int(input("Enter the number you would like to check: "))
 
+    if num < 0:
+        print(f"{num} is a negative number.")
+    else: 
+        print(f"{num} is a positive number.")
 
-if num < 0:
-    print(f"{num} is a negative number.")
-else: 
-    print(f"{num} is a positive number.")
+    repeat = input("Would you like to run again? (y/n): ")
+    if repeat == "y":
+        main()
+    elif repeat == "n":
+        exit()
+main()
