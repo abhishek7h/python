9e59b8bc1c9dd0f481ab9ffaceaa3c3696e8c51d
Abhishek.H <abhishek18h@gmail.com>
2022-12-29 13:47:04 +0530

    made a counter to practice while loops

---
+++ z_learning/while-loop/counter.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number that you want to count from: "))
+num2 = int(input("Enter the number that you want to count to: "))
+
+i = num
+while i < num2:
+    print(i)
+    i+=1
