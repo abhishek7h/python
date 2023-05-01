6eab57218573fde822392f6b0452289558299e7e
Abhishek.H <abhishek18h@gmail.com>
2023-05-01 11:37:20 +0530

    made a quadratic formula root checker

---
+++ math/quadratic-formula.py
@@ -0,0 +1,22 @@
+import math
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+quadraticFormulaPlus = -b + math.sqrt(b*b - 4*a*c)
+quadraticFormulaMinus = -b - math.sqrt(b*b - 4*a*c)
+qfp2 = quadraticFormulaPlus/2
+qfm2 = quadraticFormulaMinus/2
+# qfp22 = qfp2/2
+# qfm22 = qfm2/2
+
+
+print(f"The roots are {quadraticFormulaPlus} and {quadraticFormulaMinus}.")
+
+if quadraticFormulaPlus % 2 == 0 or quadraticFormulaMinus % 2 == 0:
+    print(
+        f"The roots can be simplified once or more than once, {qfp2}, {qfm2}")
+
+# if qfp2 % 2 and qfm2 % 2 == 0:
+#     print(f"The roots can be simplified again, {qfp22}, {qfm22}.")
