2f2a8c069fba8f7cbdab79bb079f780a73f9ba95
Abhishek.H <abhishek18h@gmail.com>
2023-06-10 09:52:05 +0530

    api

---
+++ api/api.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "John Doe",
+        "email": "john@doe.com"
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data), 200
+
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
